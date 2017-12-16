3db8071c8be5277b3745a0f3904ac6a051e507ed
Ian Fleeton <ianfleeton@gmail.com>
2017-12-16 12:54:59 +0000

    Main game loop

---
+++ TimesTablesTest/Shuffle.swift
@@ -0,0 +1,33 @@
+//
+//  Shuffle.swift
+//  TimesTablesTest
+//
+//  Created by Ian Fleeton on 16/12/2017.
+//  Copyright © 2017 Ian Fleeton. All rights reserved.
+//
+
+import Foundation
+
+// Credit to: https://stackoverflow.com/a/24029847/98792
+extension MutableCollection {
+    /// Shuffles the contents of this collection.
+    mutating func shuffle() {
+        let c = count
+        guard c > 1 else { return }
+        
+        for (firstUnshuffled, unshuffledCount) in zip(indices, stride(from: c, to: 1, by: -1)) {
+            let d: IndexDistance = numericCast(arc4random_uniform(numericCast(unshuffledCount)))
+            let i = index(firstUnshuffled, offsetBy: d)
+            swapAt(firstUnshuffled, i)
+        }
+    }
+}
+
+extension Sequence {
+    /// Returns an array with the contents of this sequence, shuffled.
+    func shuffled() -> [Element] {
+        var result = Array(self)
+        result.shuffle()
+        return result
+    }
+}
--- TimesTablesTest/TimesTablesViewController.swift
+++ TimesTablesTest/TimesTablesViewController.swift
@@ -10,19 +10,57 @@ import UIKit
 
 class TimesTablesViewController: UIViewController {
     var number = 0
-
+    @IBOutlet var question: UILabel!
+    @IBOutlet var guess: UITextField!
+    @IBOutlet var checkContinueButton: UIButton!
+    var questions: [Int] = []
+    var current = 0
+    var correct = 0
+    var explaining = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        questions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].shuffled()
+        
+        setQuestion()
     }
 
+    func setQuestion() {
+        question.text = "\(questions[current]) x \(number) ="
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func checkTapped(_ sender: Any) {
+        if (explaining) {
+            explaining = false
 
+            checkContinueButton.setTitle("check", for: .normal)
+            if (current < 11) {
+                current += 1
+                setQuestion()
+            }
+        } else {
+            explaining = true
+
+            let answer = "\(number * questions[current])"
+            let guessed = guess.text!
+            
+            checkContinueButton.setTitle("continue", for: .normal)
+
+            if guessed == answer {
+                correct += 1
+                question.text = "correct! you're awesome"
+            } else {
+                question.text = "oh no! the answer was: " + answer
+            }
+        }
+    }
+    
     /*
     // MARK: - Navigation
 
