8ff9b0f9b96b60baff22dfd55a6c854fd020134a
Ian Fleeton <ianfleeton@gmail.com>
2017-12-16 11:48:27 +0000

    Initial Commit

---
+++ TimesTablesTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TimesTablesTest
+//
+//  Created by Ian Fleeton on 16/12/2017.
+//  Copyright © 2017 Ian Fleeton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
